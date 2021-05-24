654ae7ec283efafde5ec4834d82b18197a590908
Yan Navoichyk <yan.navoichyk@lecodegroup.com>
2021-05-24 14:27:51 +0300

    HappyArray completed

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,24 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var count = 0
+        for(i in 1 until sadArray.lastIndex){
+            val sum = sadArray[i - 1] + sadArray[i + 1]
+            if(sadArray[i] > sum){
+                count++
+            }
+        }
+        val happyArray = IntArray(sadArray.size - count)
+        happyArray[0] = sadArray[0]
+        happyArray[happyArray.lastIndex] = sadArray[sadArray.lastIndex]
+        count = 1
+        for(i in 1 until sadArray.lastIndex){
+            val sum = sadArray[i - 1] + sadArray[i + 1]
+            if(sadArray[i] < sum){
+                happyArray[count] = sadArray[i]
+                count++
+            }
+        }
+        return happyArray
     }
 }
